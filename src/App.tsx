ddb329fd4a9785f71ccbf0c5d7ba8754023e763c
Sdyess <skyler.dyess@hey.com>
2022-02-02 19:57:28 -0600

    DIsable unused vars linting rule while WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import GameGrid from '../src/components/grid/GameGrid';
 import Keyboard from './components/keyboard/Keyboard';
