8f16d8afd119bdcd28a58e5a67145ea1449a8ea6
Sdyess <skyler.dyess@hey.com>
2022-02-02 19:53:26 -0600

    Add simple key clicking

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -3,11 +3,17 @@ import { KeyValue } from './KeyValue'
 
 type Props = {
     value: KeyValue
+    clickAction: any
 };
 
-const Key = ({ value }: Props) => {
+const Key = ({ value, clickAction }: Props) => {
+
+    const clickKey: React.MouseEventHandler<HTMLButtonElement> = ( event ) => {
+        clickAction(value);
+    };
+
     return (
-        <Button variant="outlined" sx={{height: "58px", margin: "0 6px 6px 0"}}>{value}</Button>
+        <Button variant="outlined" sx={{height: "58px", margin: "0 6px 6px 0"}} onClick={clickKey}>{value}</Button>
     )
 };
 
--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -1,41 +1,47 @@
 import Key from './Key';
+import { KeyValue } from './KeyValue';
+
+const onKeyClick = (value: KeyValue) => {
+    console.log(value);
+}
+
 
 const Keyboard = () => {
     return(
         <div style={{marginTop:"20px"}}>
             <div>
-                <Key value="Q" />
-                <Key value="W" />
-                <Key value="E" />
-                <Key value="R" />
-                <Key value="T" />
-                <Key value="Y" />
-                <Key value="U" />
-                <Key value="I" />
-                <Key value="O" />
-                <Key value="P" />
+                <Key value="Q" clickAction={onKeyClick} />
+                <Key value="W" clickAction={onKeyClick} />
+                <Key value="E" clickAction={onKeyClick} />
+                <Key value="R" clickAction={onKeyClick} />
+                <Key value="T" clickAction={onKeyClick} />
+                <Key value="Y" clickAction={onKeyClick} />
+                <Key value="U" clickAction={onKeyClick} />
+                <Key value="I" clickAction={onKeyClick} />
+                <Key value="O" clickAction={onKeyClick} />
+                <Key value="P" clickAction={onKeyClick} />
             </div>
             <div>
-                <Key value="A" />
-                <Key value="S" />
-                <Key value="D" />
-                <Key value="F" />
-                <Key value="G" />
-                <Key value="H" />
-                <Key value="J" />
-                <Key value="K" />
-                <Key value="L" />
+                <Key value="A"  clickAction={onKeyClick} />
+                <Key value="S"  clickAction={onKeyClick} />
+                <Key value="D"  clickAction={onKeyClick} />
+                <Key value="F"  clickAction={onKeyClick} />
+                <Key value="G"  clickAction={onKeyClick} />
+                <Key value="H"  clickAction={onKeyClick} />
+                <Key value="J"  clickAction={onKeyClick} />
+                <Key value="K"  clickAction={onKeyClick} />
+                <Key value="L"  clickAction={onKeyClick} />
             </div>
             <div>
-                <Key value="Enter" />
-                <Key value="Z" />
-                <Key value="X" />
-                <Key value="C" />
-                <Key value="V" />
-                <Key value="B" />
-                <Key value="N" />
-                <Key value="M" />
-                <Key value="Delete" />
+                <Key value="Enter" clickAction={onKeyClick} />
+                <Key value="Z" clickAction={onKeyClick} />
+                <Key value="X" clickAction={onKeyClick} />
+                <Key value="C" clickAction={onKeyClick} />
+                <Key value="V" clickAction={onKeyClick} />
+                <Key value="B" clickAction={onKeyClick} />
+                <Key value="N" clickAction={onKeyClick} />
+                <Key value="M" clickAction={onKeyClick} />
+                <Key value="Delete" clickAction={onKeyClick} />
             </div>
         </div>
     )
